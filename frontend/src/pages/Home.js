f72ce65d3177928ea93b99fac84a85f837561b07
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2024-09-10 22:15:21 -0300

    style: less parts of the lp

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,30 +1,16 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
 import "./Home.css";
 import notaDeReal1 from "../images/notasdereal1.png";
-import planilha from "../images/planilha.png";
-import computador from "../images/notebook.png";
 import plus from "../images/pngwing.com.png";
-import player from "../images/playerdevídeo.png";
-import seta from "../images/seta.png";
-import house from "../images/Group.png";
+import playerOne from "../images/Capa1.png";
+import playerTwo from "../images/Capa2.png";
 import notaDeCem from "../images/nota100real.png";
 import notaDeCem1 from "../images/nota100real1.png";
-import React, { useRef } from "react";
 
 const Home = () => {
-  const contentRef = useRef(null);
-  const contentRefMobile = useRef(null);
 
   const linkSheet = () => {
     window.open("https://pay.kiwify.com.br/BvcfJ4k", "_blank");
-  };
-
-  const handleScrollToContentRef = () => {
-    contentRef.current.scrollIntoView({ behavior: "smooth" });
-  };
-
-  const handleScrollToContentRefMobile = () => {
-    contentRefMobile.current.scrollIntoView({ behavior: "smooth" });
   };
   return (
     <>
@@ -32,7 +18,12 @@ const Home = () => {
         <div id="notaDeRealDiv">
           <img src={notaDeReal1} alt="notasdereal1.png" id="notaDeReal1"></img>
         </div>
-
+        <div className="video">
+          <iframe
+            src="https://www.youtube.com/embed/sGJ7Vo6g7bg?si=GyqNqarKdVHdXIxJ"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        </div>
         <div className="contentOneMobile">
           <div className="texts">
             <div className="textOne">TRANSFORME AS</div>
@@ -42,19 +33,8 @@ const Home = () => {
             </div>
 
             <div className="textFour">COM UMA PLANILHA</div>
-            <button onClick={handleScrollToContentRefMobile}>
-              Como Funciona?
-            </button>
-            <div className="images">
-              <div>
-                {" "}
-                <img src={planilha} alt="planilha" className="imageSheet" />
-                <img
-                  src={computador}
-                  alt="notebook"
-                  className="imageNotebook"
-                />
-              </div>
+            <div className="redirect">
+              <button onClick={linkSheet}>ADQUIRIR POR APENAS R$14,90</button>
             </div>
           </div>
         </div>
@@ -69,28 +49,24 @@ const Home = () => {
 
         <div className="contentOneDesktop">
           <div className="texts">
-            <div className="textOne">TRANSFORME AS</div>
-            <div className="textTwo">FINANÇAS</div>
-            <div className="same-line">
-              DE SUA <span>EMPRESA</span>
-            </div>
-
-            <div className="textFour">COM UMA PLANILHA</div>
-            <button onClick={handleScrollToContentRef}>Como Funciona?</button>
+            <div className="textOne">TRANSFORME AS FINANÇAS</div>
+            <div className="textTwo">DE SUA EMPRESA COM UMA PLANILHA</div>
           </div>
-          <div className="images">
-            <div>
-              {" "}
-              <img src={planilha} alt="planilha" className="imageSheet" />
-              <img src={computador} alt="notebook" className="imageNotebook" />
+          <div className="video">
+          <iframe
+            src="https://www.youtube.com/embed/sGJ7Vo6g7bg?si=GyqNqarKdVHdXIxJ"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        </div>
+        <div className="redirect">
+              <button onClick={linkSheet}>ADQUIRIR POR APENAS R$14,90</button>
             </div>
-          </div>
         </div>
         <div className="notaDeCem">
           <img src={notaDeCem} alt="" />
         </div>
       </div>
-      <div id="PartTwoMobile" ref={contentRefMobile}>
+      {/* <div id="PartTwoMobile">
         <div className="texts">
           <div className="textOne">POR QUE</div>
           <div className="textTwo">VOCÊ PRECISA</div>
@@ -121,11 +97,13 @@ const Home = () => {
             de imediato a gestão financeira profissional do seu empreendimento.
           </div>
         </div>
-        <div className="redirect" >
-          <button className="bt_carrinho" onClick={linkSheet}>Adquirir Planilha</button>
+        <div className="redirect">
+          <button className="bt_carrinho" onClick={linkSheet}>
+            Adquirir Planilha
+          </button>
         </div>
       </div>
-      <div id="PartTwoDesktop" ref={contentRef}>
+      <div id="PartTwoDesktop">
         <div className="texts">
           <div>
             <div className="textOne">POR QUE</div>
@@ -147,8 +125,10 @@ const Home = () => {
             />
           </div>
 
-          <div className="redirect" >
-            <button className="bt_carrinho" onClick={linkSheet}>Adquirir Planilha</button>
+          <div className="redirect">
+            <button className="bt_carrinho" onClick={linkSheet}>
+              Adquirir Planilha
+            </button>
           </div>
         </div>
         <div className="formats">
@@ -174,8 +154,8 @@ const Home = () => {
       </div>
       <div id="PartThreeMobile">
         <div className="texts">COMPRE AGORA E TRANSFORME O SEU NEGÓCIO!</div>
-      </div>
-      <div id="PartFourMobile">
+      </div> */}
+      {/* <div id="PartFourMobile" ref={contentRef}>
         <div className="video">
           <iframe
             src="https://www.youtube.com/embed/sGJ7Vo6g7bg?si=GyqNqarKdVHdXIxJ"
@@ -189,14 +169,16 @@ const Home = () => {
               EMPRESA? ENTENDA!
             </div>
             <div className="redirect">
-              <button className="bt_carrinho" onClick={linkSheet}>Quero a Minha Planilha</button>
+              <button className="bt_carrinho" onClick={linkSheet}>
+                Quero a Minha Planilha
+              </button>
             </div>
           </div>
           <div className="house">
             <img src={house} alt="" />
           </div>
         </div>
-      </div>
+      </div> */}
       <div id="PartFiveMobile">
         <div className="textOne">
           BÔNUS PARA VOCÊ QUE VAI ADQUIRIR A PLANILHA
@@ -212,8 +194,8 @@ const Home = () => {
         </div>
         <div className="videosAndPrices">
           <div className="images">
-            <img src={player} alt="" className="imageOne" />
-            <img src={player} alt="" className="imageTwo" />
+            <img src={playerOne} alt="" className="imageOne" />
+            <img src={playerTwo} alt="" className="imageTwo" />
           </div>
           <div className="prices">
             <div>
@@ -221,10 +203,14 @@ const Home = () => {
                 <div className="priceOne">de R$68,76</div>
                 <div>por apenas</div>
               </div>
-              <div className="biggerPrice">R$29,70</div>
+              <div className="biggerPrice">
+                R$14,<span>90</span>
+              </div>
             </div>
             <div className="redirect">
-              <button className="bt_carrinho" onClick={linkSheet}>Adquirir Planilha</button>
+              <button className="bt_carrinho" onClick={linkSheet}>
+                Adquirir Planilha
+              </button>
             </div>
           </div>
         </div>
